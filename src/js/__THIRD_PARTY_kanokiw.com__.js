700768dbe9ac0f5d2d2a6fbce6880d7d45c48545
KANOKIw <KUMANKENw@gmail.com>
2023-10-22 16:51:14 +0900

    bv

--- src/js/__THIRD_PARTY_kanokiw.com__.js
+++ src/js/__THIRD_PARTY_kanokiw.com__.js
@@ -1,8 +1,6 @@
-!function()
-{
+!function(){
     var q = "__history";
-    function g(n, u)
-    {
+    function g(n, u){
         if (!u) u = window.location.href;
         n = n.replace(/[[]]/g, "\$&");
         var r = new RegExp("[?&]" + n + "(=([^&#]*)|&|#|$)"),
@@ -11,43 +9,44 @@
         if (!s[2]) return "";
         return decodeURIComponent(s[2].replace(/\\+/g, " "));
     }
-    function v()
-    {
+    function v(){
         var d =[];
         try {
             d = JSON.parse(localStorage.getItem(q));
             if (!Array.isArray(d))
                 throw new Error();
-        } catch (e)
-        {
+        } catch (e){
             return [];
         }
         return d;
     }
-    function z(j)
-    {
+    function z(j){
         var d = v();
         d.push(j);
         localStorage.setItem(q, JSON.stringify(d));
     }
-    setInterval(function()
-        {
+    setInterval(function(){
             for (var a of document.getElementsByTagName("a")){
                 var h = a.href;
                 var t = 0;
-                if (h == null || ((typeof h === "string" || h instanceof String) && h.startsWith("http://kanokiw.com")))
-                    continue;
-                for (var p of ["http", "https"])
-                {
-                    for (var l of ["co.jp", "com"])
-                    {
+                if (
+                    h == null
+                    || (
+                        (
+                            typeof h == "string" 
+                            || h instanceof String
+                        )
+                        && h.startsWith("http://kanokiw.com")
+                        )
+                    ) continue;
+                for (var p of ["http", "https"]){
+                    for (var l of ["co.jp", "com"]){
                         if (
                             h.startsWith(p+"://www.google."+l+"/url")
                             || h.startsWith(p+"://google."+l+"/url")
                             || h.startsWith(p+"://www.youtube."+l+"/redirect")
                             || h.startsWith(p+"://youtube."+l+"/redirect")
-                        )
-                        {
+                        ){
                             var b = h;
                             h = g("url", b);
                             if (h == null)
@@ -61,18 +60,18 @@
                 }
                 var o = parent.location.href;
                 var f = "http://kanokiw.com/browse?u="+encodeURIComponent(h);;
-                if (o.startsWith("http://preview.kanokiw.com/find/") || o.startsWith("http://kanokiw.com/find/"))
-                {
+                if (
+                    o.startsWith("http://preview.kanokiw.com/find/")
+                    || o.startsWith("http://kanokiw.com/find/")
+                ){
                     f += "&w=fullscreen";
                 }
                 a.target = "_parent";
                 a.href = f;
                 a.replaceWith(a.cloneNode(true));
             }
-        },
-    15);
-    !async function(r)
-    {
+        }, 15);
+    !async function(r){
         if (r){
             var s = new URL(window.location.href).pathname.replace(/\/+$/, "").split("/").pop();
             await fetch("/iframe-data", {
@@ -80,28 +79,23 @@
                 headers: {"Content-Type": "application/json"},
                 body: JSON.stringify({id: s}),
             })
-            .then(function(r)
-                {
-                    return r.json();
-                }
-            )
+            .then(function(r){
+                return r.json();
+            })
             .then(d)
             .catch(f);
-            function d(t)
-            {
+            function d(t){
                 console.log(t)
                 var view = "http://kanokiw.com/view?s="+s;
 
-                for (var c of v())
-                {
+                for (var c of v()){
                     if (c.id == s)
                         return;
                 }
                 z({title: t.title, url: t.url, view: view,
                     favicon_url: t.favicon_url, id: s, timestamp: t.timestamp});
             }
-            function f(b)
-            {
+            function f(b){
                 
             }
         }
